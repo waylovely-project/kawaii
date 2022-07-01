ad1ac16ef25b9ef72892d33e8a3839e11ccfd2b0
Nefo Fortressia <nefothingy@hotmail.com>
2022-07-01 19:07:40 +0700

    cmake: Dehardcode the android Sdk version and set to use the Ninja backend
    Signed-off-by: Nefo Fortressia <nefothingy@hotmail.com>

--- simple_and_kawaii/scripts/cmake.sh
+++ simple_and_kawaii/scripts/cmake.sh
@@ -2,4 +2,8 @@
 
 . "$(dirname $0)/shared.sh"
 
-cmake -DCMAKE_TOOLCHAIN_FILE=$(readlink -f $ANDROID_NDK_PATH/build/cmake/android.toolchain.cmake) -DANDROID_ABI=$ANDROID_ABI -DANDROID_PLATFORM=android-29 --install-prefix=$PREFIX "$@"
+cmake -DCMAKE_TOOLCHAIN_FILE=$(readlink -f $ANDROID_NDK_PATH/build/cmake/android.toolchain.cmake) \
+-DANDROID_ABI=$ANDROID_ABI -DANDROID_PLATFORM=$ANDROID_PLATFORM \
+--install-prefix=$PREFIX -G ninja "$@"
+
+ninja install
