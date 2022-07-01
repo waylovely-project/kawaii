1fec08a8756c7c7741936117173855331ed911e5
Nefo Fortressia <nefothingy@hotmail.com>
2022-07-01 17:22:29 +0700

    Fix issues with the cyclic dependencies detection
    Signed-off-by: Nefo Fortressia <nefothingy@hotmail.com>

--- simple_and_kawaii/build.py
+++ simple_and_kawaii/build.py
@@ -6,43 +6,68 @@ import click
 from .utils import build_path, root_source_dir
 
 
+class Project:
+    def __init__(self, name: str, path: str, deps: list):
+        self.name = name
+        self.path = path
+        self.deps = deps
+        self.deps_left = deps
+
 
 @click.command()
 def build_deps():
-    """ Build native libraries required for Waylovely and Portals.\n
+    """Build native libraries required for Waylovely and Portals.\n
     Mostly they are written in C/C++. They'll get installed to the '' folder of the root directory of the Git repository!
     This behavior can be changed by changing the "deps-location" path in kawaii/config.json file. 
     """
     config = json.loads(open(path.join(build_path, "config.json")).read())
-    if hasattr(config, "deps-location"):
+    if "deps-location" in config:
         deps_folder = config["deps-location"]
     else:
         deps_folder = path.join(root_source_dir, "deps")
     projects = dict()
     queue = []
     projects_order = []
+    count = 0
     for file in os.listdir(deps_folder):
         file_path = path.join(deps_folder, file)
-        if path.isdir(file):
+        if path.isdir(file_path):
             project_config_path = path.join(file_path, "kawaii.config.json")
-            build_script_path = path.join(file_path, "kawaii-build.sh")
-            if path.exists():
+
+            if path.exists(project_config_path):
+                try:
                 project_config = json.loads(open(project_config_path).read())
+                except json.JSONDecodeError as error:
+                    click.echo(
+                        f"Oh nooo! We found an error while processing {project_config_path}! {error}",
+                        err=True,
+                    )
+                    exit(1)
 
-                if hasattr(project_config, "deps"):
-
-                    projects.add(file, {"path": file_path, "deps": project_config.deps})
-
-                    if project_config.deps.len() == 0:
-                        queue.push(file)
+                if "deps" in project_config:
+                    projects[file] = Project(file, file_path, project_config["deps"])
+                    if len(project_config["deps"]) == 0:
+                        queue.append(file)
 
     while queue:
         current = queue.pop()
         projects_order.append(current)
         for (name, config) in projects.items():
-            config.deps.remove(name)
-            if config.deps.len() == 0:
+            if current in config.deps_left:
+
+                config.deps_left.remove(current)
+
+                if len(config.deps_left) == 0:
+
                 queue.append(name)
+        count += 1
+
+    projects_with_deps = [
+        project
+        for project in filter(
+            lambda project: len(project[1].deps_left) != 0, projects.items()
+        )
+    ]
                 
     projects_with_deps = [project for project in filter(
         lambda project: project[1].deps.len() != 0, projects.items()
